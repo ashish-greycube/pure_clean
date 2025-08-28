c128a586a4870a0aced5e64da1386e51fff496cd
durve-greycube <zelam@greycube.in>
2025-08-28 11:44:01 +0530

    fix:total profit formula change for summary in customer profit based on weight report, so weight report role permission

--- pure_clean/pure_clean/report/customer_profit_based_on_weight/customer_profit_based_on_weight.py
+++ pure_clean/pure_clean/report/customer_profit_based_on_weight/customer_profit_based_on_weight.py
@@ -19,7 +19,7 @@ def execute(filters=None):
 		msgprint(_("No records found"))
 		return columns,report_data
 	
-	notes="<b>Notes: </b><br>Total Cost (SAR) Condition : In item price doctype --> customer, price list and  weight(gm) is properly set."
+	notes="<b>Notes: </b><br>(1) Total Cost (SAR) Condition : In item price doctype --> customer, price list and  weight(gm) is properly set.<br>(2) Total profit(all customer) = Total sales(all customer) - Total cost as per expense account"
 	return columns, report_data, notes, None, report_summary
 
 def get_columns():
@@ -134,7 +134,7 @@ def get_data(filters):
 	for row in data_for_all_customer:
 		if row.get('total_weight'):
 			total_cost=flt(((cost_per_weight * row.get('total_weight'))/1000),2)
-			total_profit_for_all_customer=flt(total_profit_for_all_customer+(row.get('total_sales') -total_cost),2)
+			# total_profit_for_all_customer=flt(total_profit_for_all_customer+(row.get('total_sales') -total_cost),2)
 
 	for d in result_to_show:
 		if d.get('total_weight'):
@@ -144,7 +144,7 @@ def get_data(filters):
 			d['total_weight']='---'
 			d['total_cost']='---'
 			d['profit']='---'
-		
+	total_profit_for_all_customer = total_sales_of_all_customer - total_cost_for_all_customer
 	report_summary=[
 		{'label':'Total cost as per expense account','value':total_cost_for_all_customer},
 		{'label':'Total weight(all customer)','value':str(flt((total_weight_of_all_customer/1000),2))+' Kg'},
