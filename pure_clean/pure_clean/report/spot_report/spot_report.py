445e00ca0263ea2c3c4ebe5318ab98b0dccb708b
durve-greycube <zelam@greycube.in>
2026-06-30 10:34:37 +0530

    fix: Spot report, single line for customer and item combination, spot balance calculation

--- pure_clean/pure_clean/report/spot_report/spot_report.py
+++ pure_clean/pure_clean/report/spot_report/spot_report.py
@@ -20,57 +20,68 @@ def get_columns():
             "fieldtype" : "Link",
             "label" : ("Customer"),
             "options" : "Customer",
-            "width": "300"
+            "width": "400"
         },
         {
-            "fieldname" : "item_name",
+            "fieldname" : "item_code",
             "fieldtype" : "Link",
-            "label" : ("Item Name"),
+            "label" : ("Item Code"),
             "options" : "Item",
-            "width": "180"
+            "width": "280"
         },
         {
             "fieldname" : "spot_in",
             "fieldtype" : "Int",
             "label" : ("Spot In"),
+            "width": "180"
         },
         {
             "fieldname" : "spot_out",
             "fieldtype" : "Int",
             "label" : ("Spot Out"),
+            "width": "180"
         },
         {
             "fieldname" : "balance",
             "fieldtype" : "Int",
             "label" : ("Balance"),
+            "width": "180"
         },
     ]
 
 def get_data(filters):
+    conditions = ""
+    values = {}
 
-    conditions = {}
-
+    # Safely building conditions using dictionary values to prevent SQL injection
     if filters.get("customer"):
-        conditions["customer"] = "AND so.customer = '{0}'".format(filters["customer"])
+        conditions += " AND so.customer = %(customer)s"
+        values["customer"] = filters.get("customer")
     
     if filters.get("item"):
-        conditions["item"] = "AND soi.item_code = '{0}'".format(filters["item"])
+        conditions += " AND soi.item_code = %(item)s"
+        values["item"] = filters.get("item")
     
     if filters.get("from_date") and filters.get("to_date"):
-        conditions["date"] = "AND so.transaction_date BETWEEN '{0}' AND '{1}'".format(filters["from_date"], filters["to_date"])
+        conditions += " AND so.transaction_date BETWEEN %(from_date)s AND %(to_date)s"
+        values["from_date"] = filters.get("from_date")
+        values["to_date"] = filters.get("to_date")
 
-    return frappe.db.sql("""
-		SELECT
-			so.customer AS customer,
-			soi.item_code AS item_name,
-			soi.spot_in_cf AS spot_in,
-			soi.spot_out_cf AS spot_out,
-			(soi.spot_in_cf - soi.spot_out_cf) AS balance
-		FROM
-			`tabSales Order` so
-		JOIN
-			`tabSales Order Item` soi
-		ON soi.parent = so.name
-		WHERE so.docstatus != 2
-            {0} {1} {2}
-    """.format(conditions.get("customer") or "", conditions.get("item") or "", conditions.get("date") or ""), as_dict=1, debug=1)
+    return frappe.db.sql(f"""
+        SELECT
+            so.customer AS customer,
+            soi.item_code AS item_code,
+            soi.item_name AS item_name,
+            SUM(soi.spot_in_cf) AS spot_in,
+            SUM(soi.spot_out_cf) AS spot_out,
+            (SUM(soi.spot_in_cf) - SUM(soi.spot_out_cf)) AS balance
+        FROM
+            `tabSales Order` so
+        JOIN
+            `tabSales Order Item` soi ON soi.parent = so.name
+        WHERE 
+            so.docstatus != 2
+            {conditions}
+        GROUP BY
+            so.customer, soi.item_code
+    """, values, as_dict=1, debug=1)
